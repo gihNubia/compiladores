82adcb35f6bc75f028e1b5ccc9aa3f17c6682c3e
Erick Rocha <erickfr1011@gmail.com>
2024-11-30 12:53:53 -0300

    ref(main)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,62 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import lexic.Token;
 import lexic.Lexer;
 import lexic.EndOfFileException;
 
-
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Token> tabela_de_tokens = new ArrayList<Token>();
-            if (args.length == 0 || args.length > 1) {
-                System.out.println("javac Main.java <nome do arquivo>.txt");
-            }
-            else{
-                try{
-                    BufferedReader br = new BufferedReader(new FileReader(args[0]));
-                    Lexer lexer = new Lexer(br);
+        Optional.of(args)
+                .map(Main::getFileName)
+                .map(Main::getLexer)
+                .map(Main::getTokens)
+                .ifPresent(tokens -> {
+                    tokens.map(Token::toString)
+                            .forEach(System.out::println);
+                });
+    }
+
+    private static String getFileName(final String[] args) {
+        if (args.length != 1) {
+            // O comando seria esse mesmo? javac?
+            System.out.println("javac Main.java <nome do arquivo>.txt");
+            return null;
+        }
+
+        return args[0];
+    }
+
+    private static Lexer getLexer(final String fileName) {
+        return Optional.ofNullable(fileName)
+                .<FileReader>map(fn -> {
                     try {
-                        while (true) {
-                            Token aux = lexer.scan();
-                            if (aux !=null){
-                                tabela_de_tokens.addLast(aux);
-                            }
-                        }
+                        return new FileReader(fn);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
+                        return null;
                     }
-                    catch (EndOfFileException eof){
+                })
+                .<BufferedReader>map(BufferedReader::new)
+                .<Lexer>map(Lexer::new)
+                .orElse(null);
+    }
 
-                    }
-                    catch (Exception e){
-                        System.out.printf(e.getMessage());
-                    }
-                    finally {
-                        for (Token tabelaDeToken : tabela_de_tokens) {
-                            System.out.println(tabelaDeToken.toString());
-                        }
-                    }
+    private static Stream<Token> getTokens(final Lexer lexer) {
 
-                }
-                catch (Exception e) {
-                    System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
-                }
+        return Stream.<Token>generate(() -> {
+            try {
+                return lexer.scan();
+            } catch (EndOfFileException eof) {
 
-
-
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-
+            return null;
+        }).takeWhile(tok -> tok != null);
     }
 }
